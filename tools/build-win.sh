4b9a70934510cfc8c6fafc00fd6bcfdc39e2a917
gijun han <gijun.han@gree.net>
2020-12-07 16:36:17 +0900

    update

--- tools/build-win.sh
+++ tools/build-win.sh
@@ -2,11 +2,10 @@
 WORKSPACE="$(cd "$(dirname "$0")/.." ; pwd -P)"
 echo "WORKSPACE=${WORKSPACE}"
 cd $WORKSPACE
+git pull
 echo "build-win" >> "./build/win.txt"
 git config --global user.email "gijun.han@gree.net"
 git config --global user.name "gijun.han"
-
 git add .
 git commit -m "build complete win"
-git pull
 git push -u origin main
